36a79f51a32c53c34113b2084029d8db13dbd010
Ryan Hill <hill1886@umn.edu>
2022-09-06 22:03:09 -0500

    Emit data received event to frontend on timer

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,43 @@
     windows_subsystem = "windows"
 )]
 
+use std::{sync::{Arc, Mutex}, ops::Deref};
+
+use tauri::{Manager, AppHandle, Runtime};
+
+use chrono::Duration;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn greet<R: Runtime>(app: AppHandle<R>, name: &str) -> String {
+    app.emit_all("data-received", "TEST").unwrap();
+
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// TODO: using a mutable static variable is not great; find a way to satisfy the borrow checker
+static mut APP: Option<AppHandle> = None;
+
 fn main() {
+    let timer = timer::Timer::new();
+    let count = Arc::new(Mutex::new(0));
+
+    timer.schedule_repeating(Duration::seconds(1), move || {
+        let mut count_ref = count.lock().unwrap();
+        *count_ref += 1;
+        
+        unsafe {
+            APP.as_ref().unwrap().emit_all("data-received", count_ref.deref()).unwrap();
+        }
+    }).ignore();
+    
     tauri::Builder::default()
+        .setup(move |app| {
+            unsafe {
+                APP = Some((&app).app_handle());
+            }
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,10 @@ import { render } from "solid-js/web";
 import "./style.css";
 import 'uno.css'
 import App from "./App";
+import { listen } from "@tauri-apps/api/event";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
+
+await listen("data-received", ({ payload }) => {
+    console.log(`Data received: "${payload}"`);
+});
