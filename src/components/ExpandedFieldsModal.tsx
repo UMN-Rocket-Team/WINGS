ed1e6145f14207792cc8aadef6e17d69065fb019
Jeffrey Hu <jeffreyhu8@gmail.com>
2023-01-16 23:57:38 -0600

    ExpandedFieldsModal website fixing and formatting

--- src/components/ExpandedFieldsModal.tsx
+++ src/components/ExpandedFieldsModal.tsx
@@ -1,16 +1,16 @@
 import {ModalProps} from "./ModalProvider";
 import DefaultModalLayout from "./DefaultModalLayout";
 import {For, JSX} from "solid-js";
-import {FieldInPacket, FieldsBoxProps} from "./FieldsBox";
+import {FieldInPacket, FieldsViewState} from "./FieldsView";
 import SolidChart from "./SolidChart";
 
-const ExpandedFieldsModal = (props: ModalProps<FieldsBoxProps>): JSX.Element => {
+const ExpandedFieldsModal = (props: ModalProps<FieldsViewState>): JSX.Element => {
     return (
         <DefaultModalLayout close={() => props.closeModal({})}>
-            <div class="flex flex-row gap-2">
+            <div class="flex flex-row gap-2 bg-red">
                 <For each={props.fieldsInPackets}>
                     {(fieldInPacket: FieldInPacket) =>
-                        <div class="bg-red p-2">
+                        <div class="p-2">
                             <p>{fieldInPacket.packetStructure.name} {fieldInPacket.packetStructure.fields[fieldInPacket.fieldIndex].name}</p>
                             <SolidChart/>
                         </div>
