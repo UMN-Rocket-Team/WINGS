1616772d8b8275654cb7f5619134b362463cc89d
Jeffrey Hu <jeffreyhu8@gmail.com>
2023-03-21 19:43:06 -0500

    Divided fields into packet sections in field select

--- src/components/FieldSelectModal.tsx
+++ src/components/FieldSelectModal.tsx
@@ -8,16 +8,36 @@ export type FieldSelectModalProps = {
     handleSelect: (event: Event) => void
 }
 
+const packetBackgroundColors = ["bg-red-2", "bg-gray-2"];
+
 const FieldSelectModal = (props: ModalProps<FieldSelectModalProps>): JSX.Element => {
+    const groupedFields = props.fieldViewState.fieldsInPackets.reduce((acc: Record<number, FieldInPacket[]>, fieldInPacket) => {
+        if (acc[fieldInPacket.packetId] === undefined) {
+            acc[fieldInPacket.packetId] = [];
+        }
+        acc[fieldInPacket.packetId].push(fieldInPacket);
+        return acc;
+    }, {});
+
     return (
         <DefaultModalLayout close={() => props.closeModal({})} title="Select Fields">
-            <For each={props.fieldViewState.fieldsInPackets}>
-                {(fieldInPacket: FieldInPacket) =>
-                    <div>
-                        <input type="checkbox" value={JSON.stringify([fieldInPacket.packetId, fieldInPacket.fieldIndex])} onclick={props.handleSelect}/>
-                        <label>{fieldInPacket.packetName + ": " + fieldInPacket.name}</label>
-                    </div>
-                }
+            <For each={Object.keys(groupedFields)}>
+                {(packetId: string, packetIndex) => {
+                    const packetIdNum = parseInt(packetId);
+                    return (
+                        <div class={packetBackgroundColors[packetIndex() % packetBackgroundColors.length] + " p-2"}>
+                            <h3 class="m-2">{groupedFields[packetIdNum][0].packetName}</h3>
+                            <For each={groupedFields[packetIdNum]}>
+                                {(fieldInPacket: FieldInPacket) =>
+                                    <div>
+                                        <input type="checkbox" value={JSON.stringify([fieldInPacket.packetId, fieldInPacket.fieldIndex])} onclick={props.handleSelect}/>
+                                        <label>{fieldInPacket.name}</label>
+                                    </div>
+                                }
+                            </For>
+                        </div>
+                    )
+                }}
             </For>
         </DefaultModalLayout>
     );
