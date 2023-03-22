0befff1f82fcca00026e90da1fb3ceca6c202298
Jeffrey Hu <jeffreyhu8@gmail.com>
2023-03-21 19:43:06 -0500

    Divided fields into packet sections in field select

--- src/components/FieldSelectModal.tsx
+++ src/components/FieldSelectModal.tsx
@@ -9,16 +9,44 @@ export type FieldSelectModalProps = {
     handleSelect: (event: Event) => void
 }
 
+const packetBackgroundColors = ["bg-red-2", "bg-gray-2"];
+
 const FieldSelectModal = (props: ModalProps<FieldSelectModalProps>): JSX.Element => {
+    const groupedFields = props.fieldViewState.allFieldsInPackets.reduce((acc: Record<number, FieldInPacket[]>, fieldInPacket) => {
+        if (acc[fieldInPacket.packetViewModel.id] === undefined) {
+            acc[fieldInPacket.packetViewModel.id] = [];
+        }
+        acc[fieldInPacket.packetViewModel.id].push(fieldInPacket);
+        return acc;
+    }, {});
+
     return (
         <DefaultModalLayout close={() => props.closeModal({})} title="Select Fields">
-            <For each={props.fieldViewState.allFieldsInPackets}>
-                {(fieldInPacket: FieldInPacket, index) =>
-                    <div>
-                        <input type="checkbox" value={index()} onclick={props.handleSelect}/>
-                        <label>{fieldInPacket.packetViewModel.name + ": " + (fieldInPacket.packetViewModel.components[fieldInPacket.fieldIndex].data as PacketField).name}</label>
-                    </div>
-                }
+            {/*<For each={props.fieldViewState.allFieldsInPackets}>*/}
+            {/*    {(fieldInPacket: FieldInPacket, index) =>*/}
+            {/*        <div>*/}
+            {/*            <input type="checkbox" value={index()} onclick={props.handleSelect}/>*/}
+            {/*            <label>{fieldInPacket.packetViewModel.name + ": " + (fieldInPacket.packetViewModel.components[fieldInPacket.fieldIndex].data as PacketField).name}</label>*/}
+            {/*        </div>*/}
+            {/*    }*/}
+            {/*</For>*/}
+            <For each={Object.keys(groupedFields)}>
+                {(packetId: string, packetIndex) => {
+                    const packetIdNum = parseInt(packetId);
+                    return (
+                        <div class={packetBackgroundColors[packetIndex() % packetBackgroundColors.length] + " p-2"}>
+                            <h3 class="m-2">{groupedFields[packetIdNum][0].packetViewModel.name}</h3>
+                            <For each={groupedFields[packetIdNum]}>
+                                {(fieldInPacket: FieldInPacket, index) =>
+                                    <div>
+                                        <input type="checkbox" value={index()} onclick={props.handleSelect}/>
+                                        <label>{(fieldInPacket.packetViewModel.components[fieldInPacket.fieldIndex].data as PacketField).name}</label>
+                                    </div>
+                                }
+                            </For>
+                        </div>
+                    )
+                }}
             </For>
         </DefaultModalLayout>
     );
