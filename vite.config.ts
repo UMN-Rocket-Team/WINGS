da0c6bd0d8b2f80fdab4fa8a6212a4b6a57ca4ea
Kuba Kedzior <Kedzi011@umn.edu>
2023-11-01 19:41:02 -0500

    cleaned up code

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
         externalButton: "bg-transparent dark:border-gray-4 dark:text-white border-1 border-rounded hover:bg-blue-600 hover:text-white p-1 hover:border-transparent",
         homePageButton: "flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white py-2 px-8 border-transparent border-rounded",
         redButton: "bg-red border-rounded border-0 px-4 py-2",
-        tab: "border-1 p-2 border-rounded gap-2 dark:text-gray-100 dark: dark:border-gray-4",
-        inputBox: "dark:bg-dark-700 dark:border-gray-4 dark:text-white border-1 border-rounded p-1 option:hover:blue-600",
+        tab: "border-1 p-2 border-rounded gap-2 dark:text-gray-100 dark:border-gray-4",
+        inputBox: "dark:bg-dark-700 dark:border-gray-4 dark:text-white border-1 border-rounded p-1",
         listButton: "border-transparent bg-transparent hover:bg-gray hover:bg-blue-600 hover:text-white",
         //widget shortcuts are for the boxes in the packets tab that are used to show packets and packets structures
         widgetSelected:"border-transparent bg-blue-600 text-white",
