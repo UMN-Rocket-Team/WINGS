c39e08c152fd85b8ef506fb4d6bec4123ea3f8b9
Kuba Kedzior <Kedzi011@umn.edu>
2023-11-18 21:19:58 -0600

    refactored packet_file to pass unit tests

--- src/core/packet_file_handling.tsx
+++ src/core/packet_file_handling.tsx
@@ -13,45 +13,58 @@ import { Store } from"tauri-plugin-store-api";
  */
 export const runExportPacketWindow = async (packetView: PacketViewModel) => {
     const selectedFilePath = await save({ title: 'Export Flight Data', defaultPath: packetView.name, filters: [{ name: 'FlightData', extensions: ['json'] }] });
-    exportToLocation(selectedFilePath, packetView);
+    runExport(selectedFilePath, packetView);
 }
 
+
+const runExport = async (selectedFilePath: string | null, packetView: PacketViewModel) => {
+    updatePersistentFilePaths(await exportToLocation(selectedFilePath, packetView));
+}
 /**
  * Writes a given packetViewModel to a selected File directory
  * 
  * @param selectedFilePath location where the save file will be created
  * @param packetView packetviewmodel to save to a file
  */
-export const exportToLocation = async (selectedFilePath: string | null, packetView: PacketViewModel) => {
+const exportToLocation = async (selectedFilePath: string | null, packetView: PacketViewModel) => {
     if (selectedFilePath != null) {
         let data: string = JSON.stringify(packetView);
         let filePathString: string = selectedFilePath as string;
-
-        //adds new file directory to persistent data
-        const store = new Store("persistent.dat");
-        let prevSaves: String[] | null = await store.get("recentSaves");
-        if( Array.isArray(prevSaves)){
-
-            //limit length
-            if (prevSaves.length >= 10){
-                prevSaves.shift();
-
-            }
-
-            //get rid of repeates by filtering for only elements that arent equal to new element
-            prevSaves = prevSaves.filter((value) => value != filePathString)
-
-            prevSaves.push(filePathString);
-            await store.set("recentSaves", prevSaves);
-
-        } else {
-            prevSaves = [filePathString];
-            await store.set("recentSaves", prevSaves);
-        }
-        await store.save();
         
-        await writeTextFile({ contents: data, path: filePathString, });
+        await writeTextFile({ contents: data, path: filePathString});
+        return(filePathString);
     }
+    return("");
+}
+
+/**
+ * updates a queue list of the last 10 file paths that the function has been called with
+ * this list is later accessed by a modal for future imports/exports
+ * @param filePathString file path that needs to be saved as persistent data
+ */
+const updatePersistentFilePaths = async (filePathString: string) => {
+    //adds new file directory to persistent data
+    const store = new Store("persistent.dat");
+    let prevSaves: String[] | null = await store.get("recentSaves");
+    if( Array.isArray(prevSaves)){
+
+        //limit length
+        if (prevSaves.length >= 10){
+            prevSaves.shift();
+
+        }
+
+        //get rid of repeates by filtering for only elements that arent equal to new element
+        prevSaves = prevSaves.filter((value) => value != filePathString)
+
+        prevSaves.push(filePathString);
+        await store.set("recentSaves", prevSaves);
+
+    } else {
+        prevSaves = [filePathString];
+        await store.set("recentSaves", prevSaves);
+    }
+    await store.save();
 }
 
 /**
@@ -113,18 +126,21 @@ if (import.meta.vitest) {
     const testPacketView = {id: 0, name: "testPacketView", components: [{type: PacketComponentType.Delimiter,data:{index: 0,name: "testDelimiter", identifier: "1D3NT1TY", offsetInPacket: 0}}]};
     const testDirectory = "fakeDirectory"
 
-    describe("Describe",async () => {
+    describe("Describe", async () => {
         beforeEach(async ()=> {
+            //replaces functions that read files
             vi.mock('@tauri-apps/api/fs',() => ({
                 writeTextFile: vi.fn(),
                 readTextFile: vi.fn().mockResolvedValue('{"id":0,"name":"testPacketView","components":[{"type":"Delimiter","data":{"index":0,"name":"testDelimiter","identifier":"1D3NT1TY","offsetInPacket":0}}]}')
             }))
+            //replaces functions that require user input
             vi.mock('@tauri-apps/api/dialog',() => ({
                 save: vi.fn().mockResolvedValue("fakeDirectory"),
                 open: vi.fn().mockResolvedValue("fakeDirectory")
             }))
+            //replaces functions that reachout to backend
             vi.mock('../backend_interop/api_calls',() => ({
-                addPacket: vi.fn().mockImplementation(() => "fakeDirectory")
+                addPacket: vi.fn()
             }))
             
         })
@@ -134,13 +150,13 @@ if (import.meta.vitest) {
         })
 
         it('packet_file_I/O', async () => {
-            await runImportPacketWindow();
-            expect(addPacket).toBeCalledWith(testPacketView)
-            expect(addPacket).toHaveBeenCalledTimes(1)
+            await importPacketsfromDirectories(testDirectory);
+            expect(addPacket).toBeCalledWith(testPacketView);
+            expect(addPacket).toHaveBeenCalledTimes(1);
 
-            await runExportPacketWindow(testPacketView);
-            expect(writeTextFile).toBeCalledWith({contents: testDirectoryContents,path: testDirectory})
-            expect(writeTextFile).toHaveBeenCalledTimes(1)
+            await exportToLocation(testDirectory,testPacketView);
+            expect(writeTextFile).toBeCalledWith({contents: testDirectoryContents, path: testDirectory});
+            expect(writeTextFile).toHaveBeenCalledTimes(1);
         })
     })
 }
