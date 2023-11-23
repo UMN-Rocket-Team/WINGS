03571b6dd22005b9801c5ac872e56aecccc9317f
Kuba <kedzi011@umn.edu>
2023-11-22 18:43:28 -0600

    Update src-tauri/src/commands/communication_commands.rs
    Co-authored-by: Thomas Weber <webe1023@umn.edu>

--- src-tauri/src/commands/communication_commands.rs
+++ src-tauri/src/commands/communication_commands.rs
@@ -9,7 +9,7 @@ pub fn set_active_port(
     communication_manager_state: tauri::State<'_, CommunicationManagerState>,
     port_name: &str,
 ) -> Result<(), String> {
-    use_communication_manager(serial_manager_state, &mut |communication_manager| {
+    use_communication_manager(communication_manager_state, &mut |communication_manager| {
         communication_manager.set_read_port(port_name)
     })
 }
