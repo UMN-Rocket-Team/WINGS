ece72987c1e7bae286216851c1e37546c449d0f5
Thomas Weber <webe1023@umn.edu>
2024-01-18 15:01:29 -0600

    Fix build on macOS

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,8 @@
 fn main() {
-  println!("cargo:rustc-link-lib=dylib=LibFT4222-64");
-  println!("cargo:rustc-link-lib=dylib=ftd2xx");
-  println!("cargo:rustc-link-search=native=./lib");
+  if cfg!(target_os = "windows") {
+    println!("cargo:rustc-link-lib=dylib=LibFT4222-64");
+    println!("cargo:rustc-link-lib=dylib=ftd2xx");
+    println!("cargo:rustc-link-search=native=./lib");  
+  }
   tauri_build::build()
 }
