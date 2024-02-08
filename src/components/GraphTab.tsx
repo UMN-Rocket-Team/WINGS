ea36d8e9c2b257bb332a410bbc409bd6fcb92241
8ob <adit.kadepurkar@gmail.com>
2024-02-08 11:37:59 -0600

    Two blind men sat at the end of an era, contemplating beauty. They sat atop the world's highest cliff, overlooking the land, seeing nothing.

--- src/components/GraphTab.tsx
+++ src/components/GraphTab.tsx
@@ -24,29 +24,23 @@ export type GraphProps = {
     number: number;
 };
 
-const GraphTab : Component = (): JSX.Element => {
-    let graphs: GraphStruct[];
+const GraphTab = (props: ModalProps<GraphProps>): JSX.Element => {
     return (
-        <div class="absolute z-10 top-0 left-0 bottom-0 right-0 flex flex-col bg-white dark:bg-dark-700 p-4" tabIndex={-1}
-            // Focus the root div of the modal when it is made visible so that it receives keyboard events.
-            // The root div of the modal needs to receive keyboard events so that it can close when the Escape key is pressed
-            ref={rootElement => setTimeout(() => rootElement.focus())} // Not sure why the setTimeout is necessary, but it is
-            onKeyDown={event => {
-                // Close the modal if the Escape key is pressed
-            }}>
-            {/* <b class="text-center text-4xl dark:text-white">{`Screen ${props.number}`}</b>
-            <div class="grid gap-2 h-100%" style={{"grid-auto-rows": "1fr", "grid-template-columns": `repeat(${Math.min(2, props.selectedFields.length)}, 1fr)`}}> */}
-                {/* <For each={graphs}>
-                    {(fieldInPacket: GraphStruct) =>
-                        <div class="relative">
-                            <SolidChart fieldInPacket={fieldInPacket} />
-                        </div>
-                    }
-                </For>
-                <Show when={graphs.length === 0}>
-                    <span class="inline-flex items-center justify-center">No packets to display</span>
-                </Show> */}
-            {/* </div> */}
+        <div class="flex flex-col flex-grow gap-4 border-rounded dark:text-white">
+            <div class="flex flex-grow h-0">
+                {/*Views*/}
+                <div class="grid grid-cols-1 p-2 gap-2" style={{ "width": "100%" }}>
+                    <For each={props.selectedFields}>
+                        {(fieldInPacket: GraphStruct) =>
+                            <div class="relative">
+                                <SolidChart fieldInPacket={fieldInPacket} />
+                            </div>
+                        }
+                    </For>
+                    {/* <FieldsScreen number={1} /> */}
+                </div>
+            </div>
+
         </div>
     );
 
