ddeaa78543b8ce29b7ab107901f7c792471930cd
Kuba Kedzior <Kedzi011@umn.edu>
2024-02-10 19:11:10 -0600

    we write bytes now

--- src-tauri/src/csv_manager.rs
+++ src-tauri/src/csv_manager.rs
@@ -1,18 +1,20 @@
-use std::{fs::File, path::Path};
+use std::{fs::{self, File}, path::Path};
 use anyhow::{bail, Error};
 use csv::Writer;
 
 use crate::models::packet::Packet;
 
 pub struct CSVManager {
-    writer: Writer<File>,
+    csv_writer: Writer<File>,
+    byte_writer: String
 }
 
 ///The default write path of the CSVManager is ./packetslog.csv, the program should crash if it can't write to that directory
 impl Default for CSVManager {
     fn default() -> Self {
         Self {
-            writer: csv::Writer::from_path(Path::new("../packetslog.csv")).unwrap(),
+            csv_writer: csv::Writer::from_path(Path::new("../packetslog.csv")).unwrap(),
+            byte_writer: "../rawbytes.wings".to_owned()
         }
     }
 }
@@ -23,19 +25,32 @@ impl CSVManager{
     pub fn set_write(&mut self, path: String)-> Result<(),Error> {
         match csv::Writer::from_path(Path::new(&path)) {
             Err(err) => bail!("unable to load from path {}, error: {}", path, err),
-            Ok(writer) => {self.writer = writer; Ok(())},
+            Ok(writer) => {self.csv_writer = writer; Ok(())},
         }  
     }
     
     pub fn write_packet(&mut self, packet: Packet)-> Result<(),Error> {
-        match self.writer.serialize(packet.field_data){
-            Err(_) => bail!("Unable to write packet"),
+        match self.csv_writer.serialize(packet.field_data){
+            Err(err) => bail!("Unable to write packet and got error: {}", err),
             Ok(_) => {
-                match self.writer.flush() {
-                    Err(_) => bail!("Unable to flush packet writer"),
-                    Ok(_) => Ok(()),
+                match self.csv_writer.flush() {
+                    Err(err) => bail!("Unable to flush packet writer and got error: {}", err),
+                    Ok(ok) => Ok(ok),
                 }
             },
+        }  
+    }
+
+    pub fn write_bytes(&mut self, mut data: Vec<u8>)-> Result<(),Error> {
+        let mut old_data = vec![];
+        match fs::read(self.byte_writer.clone()){
+            Err(_) => {},
+            Ok(ok) => {old_data = ok},
+        }  
+        old_data.append(&mut data);
+        match fs::write(self.byte_writer.clone(), old_data){
+            Err(err) => bail!("Unable to write packet and got error:{}", err),
+            Ok(ok) => {Ok(ok)},
         }  
     }
 }
