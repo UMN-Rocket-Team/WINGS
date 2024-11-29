a85ec94b9a99ea29c5ad688caa4961bede9c38a6
Joe Anderson <and10287@umn.edu>
2024-11-29 16:30:53 -0600

    modified boolean settings modal, reworked readout settings modal

--- src/modals/ReadoutSettingsModal.tsx
+++ src/modals/ReadoutSettingsModal.tsx
@@ -1,11 +1,13 @@
 import { ModalProps } from "./ModalProvider";
 import DefaultModalLayout from "./DefaultModalLayout";
-import { For, JSX, Show } from "solid-js";
+import { For, JSX, Show, createSignal, onMount } from "solid-js";
 import { DisplayStruct, SettingsModalProps, displays, setDisplays } from "../components/DisplaySettingsScreen";
 import { useBackend } from "../backend_interop/BackendProvider";
 import { PacketComponentType, PacketField } from "../backend_interop/types";
 import { produce } from "solid-js/store";
-import closeIcon from "../assets/close.svg";
+import settingsIcon from "../assets/settings.png";
+import infoIcon from "../assets/info-sym.svg";
+import dropdownIcon from "../assets/dropdown.svg";
 import { store } from "../core/file_handling";
 
 export interface ReadoutModalProps extends SettingsModalProps {
@@ -25,6 +27,57 @@ const ReadoutSettingsModal = (props: ModalProps<ReadoutModalProps>): JSX.Element
 
     // used to restore previous name when user enters something invalid
     let oldName = props.displayStruct.displayName;
+
+    const [displaySettings, setDisplaySettings] = createSignal(false); // Are the modal settings being displayed?
+    const [displayInfo, setDisplayInfo] = createSignal(false); // Is info about the display being displayed?
+
+    let infoIconRef: HTMLImageElement | undefined;
+    onMount(() => { // Events for hovering over info icon
+        infoIconRef?.addEventListener("mouseout", (e) => {
+            setDisplayInfo(false);
+        });
+        infoIconRef?.addEventListener("mouseover", (e) => {
+            setDisplayInfo(true);
+        });
+    });
+
+    /** handleInput will handle updating the graphs name and also catches blank inputs and reverts to previous name */
+    const handleInput = (event: Event) => {
+        const newName = (event.target as HTMLElement).textContent || '';
+        if (newName.trim() !== '') {
+            setDisplayName(newName.trim(), props.index);
+            oldName = newName.trim();
+        } else {
+            (event.target as HTMLElement).textContent = oldName;
+        }
+    };
+
+    /* handleKeyDown helps handle updating the graphName by preventing enters(newlines) */
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+        }
+    };
+
+    const setDisplayName = (newName: string, index: number) => {
+        setDisplays(produce(s => {
+            s[index].displayName = newName;
+        }));
+        store.set("display", displays);
+    }
+
+    const deleteDisplay = () => {
+        // Need to clear fields before removing display
+        setDisplays(produce(s => {
+            const struct = s[props.index] as ReadoutStruct;
+            struct.fields =[];
+        }));
+        store.set("display", displays);
+        
+        setDisplays(displays.filter((_, index) => index !== props.index));
+        store.set("display", displays);
+        props.closeModal({});
+    }
 
     const getStructField = (packetId: number, fieldIndex: number): ReadoutStructField | undefined => {
         if (props.displayStruct.packetID !== packetId) {
@@ -56,81 +109,98 @@ const ReadoutSettingsModal = (props: ModalProps<ReadoutModalProps>): JSX.Element
     };
 
     return <DefaultModalLayout close={() => props.closeModal({})} title="Select Fields">
-        <div class="flex flex-col bg-neutral-200 rounded-10 dark:bg-gray p-2">
-            <h2>
-                <input
-                    value={props.displayStruct.displayName}
-                    class="text-lg border-0 p-0 m-0 bg-transparent text-center font-bold"
-                    onChange={(e) => {
-                        setDisplays(produce(s => {
-                            const struct = s[props.index] as ReadoutStruct;
-                            const value = (e.target as HTMLInputElement).value.trim();
-                            if (value) {
-                                struct.displayName = value;
-                                oldName = value;
-                            } else {
-                                struct.displayName = oldName;
-                            }
-                        }));
-                        store.set("display", displays);
-                    }}
-                />
-            </h2>
+        <div class="flex flex-col bg-neutral-200 dark:bg-gray-700 p-4 rounded-lg relative min-w-fit">
+            <Show when={displayInfo()}>
+                <div class="absolute bg-neutral-300 top-[-1px] left-[-1px] dark:bg-neutral-700 p-4 rounded-3xl pt-12 z-[2]">
+                    <p class="max-w-prose">Monitors chosen variables and displays lights that indicate whether data satisfies input inequalities or not.</p>
+                </div>            
+            </Show>
+            
+            <div class='flex flex-row leading-none justify-between mb-4'>
+                <img alt="Info" src={infoIcon} ref={infoIconRef} draggable={false} class="relative top-0 w-[23px] dark:invert z-[3]" />
 
-            <For each={PacketStructureViewModels}>{(packetViewModel, index) => (
-                <>
-                    <h3>
-                        {packetViewModel.name}
-                    </h3>
+                <h3 contenteditable={true} class="m-2 text-center font-bold w-[82%] absolute left-[50%] translate-x-[-50%]" 
+                    onBlur={handleInput} onKeyDown={handleKeyDown}>
+                    {props.displayStruct.displayName}
+                </h3>
 
-                    <For each={packetViewModel.components.filter(i => i.type === PacketComponentType.Field)}>{(component, componentIndex) => {
-                        const packetField = component.data as PacketField;
-                        const structField = () => getStructField(packetViewModel.id, packetField.index);
-                        return <label class="flex flex-row">
-                            <input
-                                type="checkbox"
-                                checked={!!structField()}
-                                onchange={(e) => {
-                                    const target = e.target as HTMLInputElement;
-                                    setActive(packetViewModel.id, packetField.index, target.checked);
-                                }}
-                            />
+                <img alt="Settings" src={settingsIcon} draggable={false} onClick={() => setDisplaySettings(s => !s)} 
+                    class="relative top-0 w-[25px] dark:invert z-[1] cursor-pointer" />
+            </div>
 
-                            {packetField.name}
+            <Show when={displaySettings()}>
+                <div class="absolute bg-neutral-300 dark:bg-neutral-700 p-4 top-0 rounded-3xl right-0 z-[0]">
+                    <div class="relative flex items-center justify-center mt-10">
+                        <button
+                            class="rounded-lg bg-red-500 hover:bg-red-600 flex items-center justify-center p-3"
+                            onClick={() => {
+                                deleteDisplay();
+                            }}>
+                            <h3>Remove Display</h3>
+                        </button>
+                    </div>
+                </div>
+            </Show>
 
-                            <Show when={structField()}>
-                                <input
-                                    type="text"
-                                    value={structField()!.unit}
-                                    onchange={(e) => {
-                                        const target = e.target as HTMLInputElement;
-                                        setDisplays(produce(s => {
-                                            const struct = s[props.index] as ReadoutStruct;
-                                            const componentField = struct.fields.find(i => i.packetFieldIndex === packetField.index);
-                                            if (componentField) {
-                                                componentField.unit = target.value;
-                                            }
-                                        }));
-                                        store.set("display", displays);
-                                    }}
-                                />
-                            </Show>
-                        </label>
-                    }}</For>
-                </>
+            <For each={PacketStructureViewModels}>{(packetViewModel, packetIdx) => (
+                <div class='flex flex-col mb-4'>
+                    <div class='flex gap-2 leading-none w-fit cursor-pointer'
+                        onClick={() => {
+                            setDisplays(produce(s => {
+                                const struct = (s[props.index] as ReadoutStruct);
+                                struct.packetsDisplayed[packetIdx()] = !struct.packetsDisplayed[packetIdx()];
+                            }));
+                            store.set("display", displays);
+                        }}>
+                        <img alt="Dropdown" src={dropdownIcon} 
+                            class={`h-4 dark:invert`} 
+                            style={`transform: rotate(${displays[props.index]?.packetsDisplayed[packetIdx()] ? "0deg" : "270deg"});`}
+                            draggable={false}/>
+                        <h3 class='font-bold'>{packetViewModel.name}</h3>
+                    </div>
+
+                    <Show when={displays[props.index]?.packetsDisplayed[packetIdx()]}>
+                        <div class='flex flex-col bg-neutral-200 dark:bg-gray-700 p-4 rounded-lg'>
+                            <For each={packetViewModel.components.filter(i => i.type === PacketComponentType.Field)}>{(component) => {
+                                const packetField = component.data as PacketField;
+                                const structField = () => getStructField(packetViewModel.id, packetField.index);
+                                return <label class="flex flex-row cursor-pointer">
+                                    <input
+                                        type="checkbox"
+                                        class="mr-1 cursor-pointer"
+                                        checked={!!structField()}
+                                        onchange={(e) => {
+                                            const target = e.target as HTMLInputElement;
+                                            setActive(packetViewModel.id, packetField.index, target.checked);
+                                        }}
+                                    />
+
+                                    {packetField.name}
+
+                                    <Show when={structField()}>
+                                        <input
+                                            type="text"
+                                            value={structField()!.unit}
+                                            class="ml-2"
+                                            onchange={(e) => {
+                                                const target = e.target as HTMLInputElement;
+                                                setDisplays(produce(s => {
+                                                    const struct = s[props.index] as ReadoutStruct;
+                                                    const componentField = struct.fields.find(i => i.packetFieldIndex === packetField.index);
+                                                    if (componentField) {
+                                                        componentField.unit = target.value;
+                                                    }
+                                                }));
+                                                store.set("display", displays);
+                                            }}
+                                        />
+                                    </Show>
+                                </label>
+                            }}</For>
+                        </div>
+                    </Show>
+                </div>
             )}</For>
-        </div>
-
-        <div class="flex items-center justify-center">
-            <button
-                class="w-[10%] h-[10%] rounded-5 border-none text-center"
-                onClick={() => {
-                    setDisplays(displays.filter((graph, index) => index !== props.index));
-                    store.set("display", displays);
-                    props.closeModal({});
-                }}>
-                <img alt="Delete" src={closeIcon} class="w-full h-full dark:invert justify-center" draggable={false} />
-            </button>
         </div>
     </DefaultModalLayout>;
 };
