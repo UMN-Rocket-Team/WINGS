a9b8e28e6686c51b9cb2f2fe713379eac17d0f7a
Rohan <ramma006@umn.edu>
2025-01-31 19:38:12 -0600

    Completed csv reader driver, has not been tested

--- src-tauri/src/communication_drivers/csv_reader_driver.rs
+++ src-tauri/src/communication_drivers/csv_reader_driver.rs
@@ -42,8 +42,8 @@ impl CommsIF for CSVReadDriver {
     } 
     //Reads a line of data from a csv file into a data packet of a specificed type
     fn parse_device_data(&mut self, raw_data_vector: &mut Vec<u8>, packet_vector: &mut Vec<Packet>) -> anyhow::Result<()> {
-        let packet_id:&Vec<u8> = &vec![0];
-        let mut good_file = match self.file { //make sure the file object is valid
+        let packet_id:usize = 0;
+        let good_file = match &self.file { //make sure the file object is valid
             Some(file) => file,
             None => return Err(anyhow::anyhow!("Invalid File")),
         };
@@ -52,22 +52,16 @@ impl CommsIF for CSVReadDriver {
         if !reader.read_byte_record(&mut field_byte_data)? {//reads one row of the csv file into the ByteRecord
             return Err(anyhow::anyhow!("Reached End of File"))
         }
-        let mut field_data = match csv::StringRecord::from_byte_record(field_byte_data) { //converts from ByteRecord to string record
+        let field_data = match csv::StringRecord::from_byte_record(field_byte_data) { //converts from ByteRecord to string record
             Ok(value) => value,
             Err(_) => return Err(anyhow::anyhow!("Bytefile does not contain valid utf-8")),
         };
-        let mut good_structure = match self.config.packet_structure_manager.get_packet_structure(packet_id) {
+        let good_structure = match self.config.packet_structure_manager.get_packet_structure(packet_id) {
             Ok(structure) => structure, //make sure the packet id returned a real structure
             Err(_) => return Err(anyhow::anyhow!("Invalid Packet")),
         };
-        let mut result:Vec<f64> = vec![0.; good_structure.size()]; 
-        for delimiter in &good_structure.delimiters {
-            let bytes = &delimiter.identifier;
-            for i in 0..bytes.len() {
-                result[delimiter.offset_in_packet + i] = bytes[i];
-                }
-            }
-        for field in good_structure.fields {
+        let mut result:Vec<PacketFieldValue> = vec![]; 
+        for field in good_structure.fields.iter() {
             let given_value = match field_data.get(field.index) {
                 Some(value) => value,
                 None => return Err(anyhow::anyhow!(format!("Field {} refers to missing index: {}", field.name, field.index)))
@@ -76,13 +70,9 @@ impl CommsIF for CSVReadDriver {
                 Ok(value) => value,
                 Err(_)=> return Err(anyhow::anyhow!(format!("Field {} refers to missing index: {}", field.name, field.index))),
             };
-            let bytes:Vec<u8> = parsed_value.to_le_bytes();
-            for i in 0..field.r#type.size() {
-                // guaranteed to not panic due to buffer size calculation previously
-                result[field.offset_in_packet + i] = bytes[i];
-            }
+            result.push(parsed_value);
         }
-        let mut new_packet = Packet{structure_id:packet_id,field_data:PacketFieldValue:UnsignedByte(result),field_meta_data:vec![]};
+        let new_packet = Packet{structure_id:packet_id,field_data:result,field_meta_data:vec![]};
         packet_vector.push(new_packet);
         Ok(())
     }
@@ -105,3 +95,8 @@ impl CommsIF for CSVReadDriver {
         Ok(())
     }
 } 
+
+#[cfg(test)]
+mod tests {
+    
+}
