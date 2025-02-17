cb6a489d2585d682fd9f1efa68b9e59316bad172
Joe Anderson <and10287@umn.edu>
2025-02-16 18:06:34 -0600

    wrapped DisplaysProvider component around App

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Homepage from "./tabs/Homepage";
 import { ModalProvider } from "./modals/ModalProvider";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { Router, Route } from "@solidjs/router"
+import { DisplaysProvider } from "./components/DisplaysProvider";
 
 const App = (): JSX.Element => {
     return (
         <ThemeProvider>
             <BackendProvider>
-                <ModalProvider>
-                    <Router>
-                        <Route path="/" component={Homepage} />
-                        <Route path="/newFlight" component={TabPage} />
-                    </Router>
-                </ModalProvider>
+                <DisplaysProvider>
+                    <ModalProvider>
+                        <Router>
+                            <Route path="/" component={Homepage} />
+                            <Route path="/newFlight" component={TabPage} />
+                        </Router>
+                    </ModalProvider>
+                </DisplaysProvider>
             </BackendProvider>
         </ThemeProvider>
     );
