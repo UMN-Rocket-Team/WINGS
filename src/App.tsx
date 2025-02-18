1183d017bd3cab775db6ee152a367e1d1b2904ef
Kuba Kedzior <Kedzi011@umn.edu>
2025-02-17 20:59:38 -0600

    file path changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { JSX } from "solid-js";
 import { BackendProvider } from "./backend_interop/BackendProvider";
 import TabPage from "./tabs/TabPage";
 import Homepage from "./tabs/Homepage";
-import { ModalProvider } from "./modals/ModalProvider";
-import { ThemeProvider } from "./components/ThemeProvider";
+import { ModalProvider } from "./core/ModalProvider";
+import { ThemeProvider } from "./theme/ThemeProvider";
 import { Router, Route } from "@solidjs/router"
 
 const App = (): JSX.Element => {
