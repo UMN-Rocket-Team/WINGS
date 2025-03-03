684c9b3bf1a7e523b0c10283fdbe5349ec322689
Joe Anderson <and10287@umn.edu>
2025-03-02 18:23:41 -0600

    fix GraphWindow

--- src/components/GraphWindow.tsx
+++ src/components/GraphWindow.tsx
@@ -1,7 +1,7 @@
 import { Component, For, JSX } from "solid-js";
 import { useParams } from "@solidjs/router";
-import { DisplayStruct, displayArray } from "./DisplaySettingsScreen";
 import { DisplaysContextValue, useDisplays } from "./DisplaysProvider";
+import { displayRegistry, DisplayStruct } from "../core/display_registry";
 
 const DisplayTab: Component = (): JSX.Element => {
     const params = useParams();
@@ -11,12 +11,15 @@ const DisplayTab: Component = (): JSX.Element => {
         <div class="flex flex-col flex-grow gap-4 rounded-lg dark:text-white">
             {/* Views */}
             <div class="grid gap-2 h-full" style={{ "grid-auto-rows": "1fr", "grid-template-columns": `repeat(${Math.min(2, displays.length)}, 1fr)` }}>
-                <For each={displays.filter(d => d.displayID === params.id)}>
+                <For each={displays}>
                     {(display: DisplayStruct) => {
-                        let DisplayElement = displayArray[display.displayElement ?? 0];
+                        console.log(display.displayID);
+                        const typeDef = displayRegistry.get(display.type)!;
+                        const DisplayComponent = typeDef?.displayComponent;
+
                         return (
                             <div class="relative" style={{ height: '40vh' }}>
-                                <DisplayElement {...display} />
+                                <DisplayComponent {...display} />
                             </div>
                         );
                     }}
