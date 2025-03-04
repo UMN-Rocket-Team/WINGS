f96850c515f8ee1c59743296a6108f78698d8cea
Joe Anderson <and10287@umn.edu>
2025-03-03 23:01:26 -0600

    add Kuba's fix to DisplaysProvider

--- src/components/DisplaysProvider.tsx
+++ src/components/DisplaysProvider.tsx
@@ -1,10 +1,7 @@
 import { createContext, onMount, ParentComponent, useContext } from "solid-js";
 import { createStore, SetStoreFunction } from "solid-js/store";
 import { store } from "../core/file_handling";
-import { GraphStruct } from "../modals/GraphSettingsModal";
-import { ReadoutStruct } from "../modals/ReadoutSettingsModal";
-import { BooleanStruct } from "../modals/BooleanSettingsModal";
-import { DisplayStruct } from "../core/display_registry";
+import { displayRegistry, DisplayStruct } from "../core/display_registry";
 
 export type DisplaysContextValue = {
     /**
@@ -26,39 +23,23 @@ export const DisplaysProvider: ParentComponent = (props) => {
     const [displays, setDisplays] = createStore<DisplayStruct[]>([]);
 
     onMount(async () => {
-        /**
-         * a store of all displays currently on the frontend
-         */
-        let importedDisplays: DisplayStruct[] = await store.get("display") ?? [];
+        const rawDisplays: unknown[]= await store.get("display") ?? [];
 
-        //safety check to remove any non-expected display types
-        for (let displayString in importedDisplays){
-            let display = importedDisplays[displayString];
-    
-            if (display.type === `Graph`){
-                let graph = display as GraphStruct;
-                if(graph.settingsModal !== 0 || graph.displayElement !== 0 || graph.x === undefined || graph.y === undefined || graph.colors === undefined){
-                    importedDisplays.splice(importedDisplays.indexOf(display),1);
-                }
+        const validatedDisplays = rawDisplays
+          .filter((d: unknown): d is DisplayStruct => 
+            typeof d === "object" && 
+            d !== null && 
+            "type" in d && 
+            displayRegistry.has((d as DisplayStruct).type)
+          )
+          .map(d => {
+            const typeDef = displayRegistry.get(d.type)!;
+            const instance = new typeDef.structClass();
+            Object.assign(instance, d);
+            return instance;
+          });
 
-            } else if (display.type === `Readout`){
-                let read = display as ReadoutStruct;
-                if(read.settingsModal !== 1 || read.displayElement !== 1 || read.fields === undefined){
-                    importedDisplays.splice(importedDisplays.indexOf(display),1);
-                }
-
-            } else if (display.type === `Indicator`) {
-                let read = display as BooleanStruct;
-                if(read.settingsModal !== 2 || read.displayElement !== 2 || read.fields === undefined){
-                    importedDisplays.splice(importedDisplays.indexOf(display),1);
-                }
-            }
-            else{
-                console.log(importedDisplays.indexOf(display));
-                importedDisplays.splice(importedDisplays.indexOf(display),1);
-            }
-        }
-        setDisplays(importedDisplays);
+        setDisplays(validatedDisplays);
     });
 
 
