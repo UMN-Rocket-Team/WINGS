1573173fd94819248c2dd127e04683047b412dad
Thomas Weber <webe1023@umn.edu>
2025-03-24 20:40:26 -0500

    flexview, no editor, display screen integration

--- src/tabs/DisplayTab.tsx
+++ src/tabs/DisplayTab.tsx
@@ -1,14 +1,70 @@
 import { Component, For, JSX, Show } from "solid-js";
-import { displays } from "../components/DisplaySettingsScreen";
+import { FlexviewObject, flexviewObjects } from "../components/DisplaySettingsScreen";
 import GraphDisplayElement from "../components/SolidChart";
 import ReadoutDisplayElement from "../components/Readout";
 import { displayRegistry, DisplayStruct, DisplayTypeDefinition } from "../core/display_registry";
+
+const RecursiveFlexviewViewer = (props: {
+    object: FlexviewObject
+}) => {
+    if (props.object.type === 'display') {
+        const display = props.object;
+        const typeDef = displayRegistry.get(display.struct.type)!;
+        const DisplayComponent = typeDef?.displayComponent;
+        return (
+            <div
+                class="w-full h-full flex items-center justify-center border border-2 border-gray p-2"
+            >
+                <DisplayComponent {...display.struct} />
+            </div>
+        );
+    }
+
+    if (props.object.type === 'layout') {
+        const layout = props.object;
+        return (
+            <div
+                class="w-full h-full flex items-center justify-center border border-2 border-gray p-2 gap-2"
+                style={{
+                    "flex-direction": layout.direction
+                }}
+            >
+                <Show
+                    when={layout.children.length > 0}
+                    fallback={(
+                        <p>Empty layout</p>
+                    )}
+                >
+                    <For each={layout.children}>{(childObjectId, childObjectIndex) => <>
+                        <div
+                            style={layout.direction === 'column' ? {
+                                width: '100%',
+                                height: `${layout.weights[childObjectIndex()] * 100}%`
+                            } : {
+                                width: `${layout.weights[childObjectIndex()] * 100}%`,
+                                height: '100%'
+                            }}
+                        >
+                            <RecursiveFlexviewViewer
+                                object={flexviewObjects[childObjectId]}
+                            />
+                        </div>
+                    </>}</For>
+                </Show>
+            </div>
+        );
+    }
+
+    return (
+        <div>Unknown flexview object</div>
+    );
+};
 
 const DisplayTab: Component = (): JSX.Element => {
     return (
         <div class="flex flex-col flex-grow gap-4 rounded-lg dark:text-white">
             {/* Views */}
-            <div class="grid gap-2 h-full" style={{ "grid-auto-rows": "1fr", "grid-template-columns": `repeat(${Math.min(2, displays.length)}, 1fr)` }}>
+            {/* <div class="grid gap-2 h-full" style={{ "grid-auto-rows": "1fr", "grid-template-columns": `repeat(${Math.min(2, displays.length)}, 1fr)` }}>
                 <For each={displays}>
                     {(display: DisplayStruct) => {
                         const typeDef = displayRegistry.get(display.type)!;
@@ -21,7 +77,10 @@ const DisplayTab: Component = (): JSX.Element => {
                         );
                     }}
                 </For>
-            </div>
+            </div> */}
+            <RecursiveFlexviewViewer
+                object={flexviewObjects[0]}
+            />
         </div>
     );
 };
