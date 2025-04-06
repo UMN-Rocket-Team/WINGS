3f4baf027122723a46f51ff3f29393884a09dcba
Joe Anderson <and10287@umn.edu>
2025-04-06 18:33:24 -0500

    create OscilloscopeChart and it's settings modal

---
+++ src/components/OscilloscopeChart.tsx
@@ -0,0 +1,225 @@
+import { Component, createEffect, onCleanup, onMount } from "solid-js";
+import { CategoryScale, Chart, ChartConfiguration, ChartTypeRegistry, LineController, LineElement, Point, PointElement, LinearScale, TimeScale, Title, Tooltip } from "chart.js";
+import zoomPlugin from 'chartjs-plugin-zoom';
+import 'chartjs-adapter-luxon';
+import { useBackend } from "../backend_interop/BackendProvider";
+import { unDecimatedPackets } from "../backend_interop/buffers";
+import { PacketComponentType, PacketField } from "../backend_interop/types";
+import { OscilloscopeGraphStruct } from "../modals/OscilloscopeGraphSettingsModal";
+
+// Register the necessary components with ChartJS so that they can be used later
+// Note: any components that are not registered here will act like no-ops if they are attempted to be used later!
+Chart.register(LineController, CategoryScale, LinearScale, TimeScale, PointElement, LineElement, Title, Tooltip, zoomPlugin);
+
+/**
+ * A component that displays the parsed data for a given OscilloscopeGraphStruct in a line chart
+ * 
+ * @param graph an OscilloscopeGraphStruct that is the graph that is being created
+ */
+const OscilloscopeGraphDisplayElement: Component<OscilloscopeGraphStruct> = (props) => {
+    // Type guard
+    if (props.type !== "graph") return <div>Invalid graph configuration</div>;
+
+    // Safe cast after type check
+    const graph = props as OscilloscopeGraphStruct;
+
+
+    const { parsedPacketCount, PacketStructureViewModels } = useBackend();
+
+    let containerElement: HTMLDivElement;
+    let canvas: HTMLCanvasElement;
+    let chart: Chart;
+
+    // Decimation variables:
+    var ptr1 = 1;
+    var ptr2 = 1;
+    var wall = 500;
+    var multiple = 2;
+    var next = 1;
+
+    const colors: string[] = ["#FFD700", "black", "blue", "red"];
+    //adds an empty array if we haven't received data in the packet type we want
+    if (unDecimatedPackets[graph.packetID] === undefined) {
+        unDecimatedPackets[graph.packetID] = [];
+    }
+
+    const initialunDecimatedPackets = unDecimatedPackets[graph.packetID];
+    const ratio = initialunDecimatedPackets.length / 100;
+    let datasets = []
+    for (let i = 0; i < graph.y.length; i++) {
+        const dataValue = {
+            label: ((PacketStructureViewModels.find(psViewModel => (psViewModel.id === graph.packetID))?.components.find(component => component.type === PacketComponentType.Field && (component.data as PacketField).index === graph.y[i]))?.data as PacketField).name,
+            data: initialunDecimatedPackets.map(packetData => ({ x: packetData.fieldData[graph.x], y: packetData.fieldData[graph.y[i]] })) ?? [],
+            backgroundColor: graph.colors[i % graph.colors.length],
+            borderColor: graph.colors[i % graph.colors.length],
+            spanGaps: true,
+        };
+        datasets.push(dataValue);
+    }
+    const data = { datasets };
+
+    const resizeObserver = new ResizeObserver((changes) => {
+        for (const change of changes) {
+            chart.resize(change.contentRect.width, change.contentRect.height);
+        }
+    });
+
+    const config: ChartConfiguration<keyof ChartTypeRegistry, Point[], unknown> = {
+        type: "line",
+        data: data,
+        options: {
+            // Chart.js' responsive mode is not very good. Instead, we manually use
+            // chart.resize() to update the chart's size to match a container element
+            // that we control. Fixes issue where the size of the graph could only
+            // increase and never go down.
+            responsive: false,
+
+            maintainAspectRatio: false,
+            animation: false,
+            parsing: false,
+            normalized: true,
+            interaction: {
+                mode: 'nearest',
+                axis: 'x',
+                intersect: false
+            },
+            plugins: {
+                decimation: {
+                    enabled: true,
+                    algorithm: "lttb",
+                },
+                title: {
+                    display: true,
+                    text: graph.displayName,
+                },
+                zoom: {
+                    pan: {
+                        enabled: true,
+                        mode: 'x'
+                    },
+                    zoom: {
+                        pinch: {
+                            enabled: true
+                        },
+                        wheel: {
+                            enabled: true
+                        },
+                        mode: 'x',
+                        drag: {
+                            enabled: true,
+                            modifierKey: 'ctrl'
+                        }
+                    },
+                    limits: {
+                        x: {
+                            minDelay: 0,
+                            maxDelay: 4000,
+                            minDuration: 1000,
+                            maxDuration: 20000
+                        }
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    type: "time",
+                    time: {
+                        unit: 'second',
+                        displayFormats: {
+                            second: 'HH:mm:ss'
+                        }
+                    },
+                    display: true,
+                    //     min: -90,
+                    //     max: 50
+                    // },
+                    // y: {
+                    //     min: -90,
+                    //     max: 50
+                }
+            },
+        }
+    };
+
+    let lastPacketCount = initialunDecimatedPackets?.length ?? 0;
+
+    // // Add new data to the chart whenever new data is parsed by the packet parser
+    createEffect(() => {
+        // Update this effect whenever the parsed packet count changes
+        const _unused = parsedPacketCount();
+
+        if (unDecimatedPackets[graph.packetID] === undefined) {
+            unDecimatedPackets[graph.packetID] = [];
+        }
+        const packetData = unDecimatedPackets[graph.packetID];
+
+        if (packetData === undefined || chart === undefined) {
+            return;
+        }
+        for (let i = 0; i < datasets.length; i++) {
+            // config.data.datasets[i].data.push(...packetData.slice(lastPacketCount).map(packetData => ({ x: packetData.fieldData[graph.x], y: packetData.fieldData[graph.y[i]] })));
+            config.data.datasets[i].data = packetData.map(packetData => ({ x: packetData.fieldData[graph.x], y: packetData.fieldData[graph.y[i]] }));
+            // console.log(config.data.datasets[i].data.length);
+        }
+
+
+        lastPacketCount = packetData.length;
+        chart.update();
+    }, { defer: true });
+
+    onMount(() => {
+        // canvas is set by ref, so elements will be defined by this point
+        chart = new Chart(canvas!, config);
+        resizeObserver.observe(containerElement!);
+
+        if (unDecimatedPackets[graph.packetID] === undefined) {
+            unDecimatedPackets[graph.packetID] = [];
+        }
+        const packetData = unDecimatedPackets[graph.packetID];
+        if (packetData === undefined) {
+            return;
+        }
+
+        // Adds previous data
+        for (let i = 0; i < datasets.length; i++) {
+            console.log(config.data.datasets[i].data);
+        }
+        lastPacketCount = packetData.length;
+        chart.update();
+
+    });
+
+    onCleanup(() => {
+        chart?.destroy();
+        resizeObserver.disconnect();
+    });
+
+    return (
+        <div
+            ref={containerElement!}
+            // Using absolute here means that the canvas' size will not affect
+            // the size of the container it's in. This prevents getting into a
+            // situation where the container can never shrink because the canvas
+            // has a fixed size applied to it.
+            class="relative w-full h-full"
+        >
+            <canvas
+                ref={canvas!}
+                // Using absolute positioning prevents the canvas' size from affecting
+                // the size of the container it is in. Chart.js will give the canvas a
+                // fixed width so that would prevent the container from shrinking.
+                class="absolute"
+            />
+            <button
+                class="absolute top-2 right-2 bg-gray-500 text-white p-3 text-xs rounded hover:bg-gray-600"
+                onClick={() => chart?.resetZoom('none')}
+            >
+                Reset Zoom
+            </button>
+        </div>
+    );
+};
+
+// TODO WORK ON ADDING A LEGEND AND BETTER COLOR CHANGING!
+
+export default OscilloscopeGraphDisplayElement;
---
+++ src/modals/OscilloscopeGraphSettingsModal.tsx
@@ -0,0 +1,239 @@
+import { ModalProps } from "../core/ModalProvider";
+import DefaultModalLayout from "../core/DefaultModalLayout";
+import { For, JSX, createSignal, Show, onMount } from "solid-js";
+import { SettingsModalProps, displays, setDisplays } from "../components/DisplaySettingsScreen";
+import { useBackend } from "../backend_interop/BackendProvider";
+import { PacketComponent, PacketComponentType, PacketField, PacketStructureViewModel } from "../backend_interop/types";
+import settingsIcon from "../assets/settings.png";
+import infoIcon from "../assets/info-sym.svg";
+import dropdownIcon from "../assets/dropdown.svg"
+import { createStore, produce } from "solid-js/store";
+import { store } from "../core/file_handling";
+import { DisplayStruct } from "../core/display_registry";
+
+export class OscilloscopeGraphStruct implements DisplayStruct {
+    displayName = `Graph`;
+    packetID = -1;
+    type = `graph`;
+    packetsDisplayed: boolean[] = [false];
+    x = 0;
+    y = [0];
+    colors = ["#FFD700", "#0000FF", "#000000", "#FF0000", "#00FF00"];
+}
+
+/**
+ * A modal component that allows a user to modify the fields contained in a screen.
+ * 
+ * @param props an object that contains a function to close the modal, the list of fields that are selected, and a callback to select a field
+ */
+const OscilloscopeGraphSettingsModal = (props: ModalProps<SettingsModalProps>): JSX.Element => {
+    if (props.displayStruct.type !== "graph") return null;
+
+    const { PacketStructureViewModels } = useBackend();
+
+    /** Signal used to help handleInput revert from blank inputs to most recent name */
+    const [graphCurrName, setName] = createSignal(props.displayStruct.displayName);
+
+    const [displayStruct, setDisplayStruct] = createStore(props.displayStruct as OscilloscopeGraphStruct);
+
+    const [displaySettings, setDisplaySettings] = createSignal(false); // Are the modal settings being displayed?
+    const [displayInfo, setDisplayInfo] = createSignal(false); // Is info about the display being displayed?
+
+    let infoIconRef: HTMLImageElement | undefined;
+    onMount(() => { // Events for hovering over info icon
+        infoIconRef?.addEventListener("mouseout", () => {
+            setDisplayInfo(false);
+        });
+        infoIconRef?.addEventListener("mouseover", () => {
+            setDisplayInfo(true);
+        });
+    });
+
+    /** handleInput will handle updating the graphs name and also catches blank inputs and reverts to previous name */
+    const handleInput = (event: Event) => {
+        const newName = (event.target as HTMLElement).textContent || '';
+        if (newName.trim() !== '') {
+            setGraphName(newName.trim(), props.index);
+            setName(newName.trim());
+        } else {
+            (event.target as HTMLElement).textContent = graphCurrName();
+        }
+    };
+
+    /* handleKeyDown helps handle updating the graphName by preventing enters(newlines) */
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+        }
+    };
+
+    const handleSelectY = (isChecked: boolean, fieldIndex: number, graphIndex: number, packet_id: number) => {
+        if (isChecked) {
+            setDisplays(produce((s) => {
+                if (s[graphIndex]!.packetID != packet_id) {
+                    (s[graphIndex] as OscilloscopeGraphStruct).y = [];
+                    s[graphIndex]!.packetID = packet_id;
+                    (s[graphIndex] as OscilloscopeGraphStruct).x = 0; //sets x back to 0 to avoid overflow problems
+                }
+                (s[graphIndex] as OscilloscopeGraphStruct).y.push(fieldIndex);
+            }));
+        } else {
+            setDisplays(produce((s) =>
+                (s[graphIndex] as OscilloscopeGraphStruct).y = (s[graphIndex] as OscilloscopeGraphStruct).y.filter(ind => ind != fieldIndex)));
+        }
+        store.set("display", displays);
+    }
+
+    const handleSelectX = (isChecked: boolean, fieldIndex: number, graphIndex: number, packet_id: number) => {
+        if (isChecked) {
+            setDisplays(produce((s) => {
+                if (s[graphIndex]!.packetID != packet_id) {
+                    (s[graphIndex] as OscilloscopeGraphStruct).y = (s[graphIndex] as OscilloscopeGraphStruct).y.filter(_ => false); //sets all y values to false
+                    s[graphIndex]!.packetID = packet_id;
+                }
+                (s[graphIndex] as OscilloscopeGraphStruct).x = fieldIndex;
+            }));
+        } else {
+            setDisplays(produce((s) =>
+                (s[graphIndex] as OscilloscopeGraphStruct).x = 0));
+        }
+        store.set("display", displays);
+    }
+
+    const setGraphName = (newName: string, index: number) => {
+        setDisplays(produce((s) =>
+            s[index]!.displayName = newName));
+        store.set("display", displays);
+    }
+
+    const deleteGraph = (index: number) => {
+        let newGraphs: DisplayStruct[] = [];
+        for (let i = 0; i < displays.length; i++) {
+            if (index !== i) {
+                newGraphs.push(displays[i]!);
+            }
+        }
+        setDisplays(newGraphs);
+        store.set("display", displays);
+    }
+
+    const updateColor = (color: string, colorIndex: number, graphIndex: number) => {
+        setDisplays(produce((s) =>
+            (s[graphIndex] as OscilloscopeGraphStruct).colors[colorIndex] = color));
+        store.set("display", displays);
+    }
+    return (
+        <DefaultModalLayout close={() => props.closeModal({})} title="Select Fields">
+
+            <div class='flex flex-col bg-neutral-200 dark:bg-gray-700 p-4 rounded-lg relative min-w-fit'>
+                <Show when={displayInfo()}>
+                    <div class="absolute bg-neutral-300 top-[-1px] left-[-1px] dark:bg-neutral-700 p-4 rounded-3xl pt-12 z-[2]">
+                        Customizable graph for visualizing data. Ability to zoom in on data, drag through data. Hold ctrl + drag over display for drag and zoom.
+                    </div>
+                </Show>
+
+                <div class='flex flex-row leading-none justify-between mb-4'>
+                    <img alt="Info" src={infoIcon} ref={infoIconRef} draggable={false} class="relative top-0 w-[23px] dark:invert z-[3]" />
+
+                    <h3 contenteditable={true} class="m-2 text-center font-bold w-[82%] absolute left-[50%] translate-x-[-50%]"
+                        onBlur={handleInput} onKeyDown={handleKeyDown}>
+                        {graphCurrName()}
+                    </h3>
+
+                    <img alt="Settings" src={settingsIcon} draggable={false} onClick={() => setDisplaySettings(s => !s)}
+                        class="relative top-0 w-[25px] dark:invert z-[1] cursor-pointer" />
+                </div>
+
+                <Show when={displaySettings()}>
+                    <div class="absolute bg-neutral-300 dark:bg-neutral-700 p-4 top-0 rounded-3xl right-0 z-[0]">
+                        {/* Graph Colors */}
+                        <div class="flex flex-col bg-neutral-300 dark:bg-neutral-700 p-4 text-center">
+                            <h2 class="font-bold">Graph Colors</h2>
+                            <For each={PacketStructureViewModels.find(psViewModel => psViewModel.id === props.displayStruct.packetID)?.components.filter(component => component.type === PacketComponentType.Field)}>
+                                {(packetComponent: PacketComponent, i) => {
+                                    const field = packetComponent.data as PacketField;
+                                    return (
+                                        <label class="flex items-center justify-center space-x-2">
+                                            {field.name}
+                                            <input type="color" class="rounded-full" value={(props.displayStruct as OscilloscopeGraphStruct).colors[i() % (props.displayStruct as OscilloscopeGraphStruct).colors.length]} onInput={(event) => {
+                                                updateColor((event.target as HTMLInputElement).value, i(), props.index);
+                                            }} />
+                                        </label>
+                                    );
+                                }}
+                            </For>
+                        </div>
+                    </div>
+                </Show>
+
+                <For each={PacketStructureViewModels}>
+                    {(PacketStructureViewModel: PacketStructureViewModel, packetIdx) =>
+                        <div class='flex flex-col mb-4'>
+                            <div class='flex gap-2 leading-none w-fit cursor-pointer'
+                                onClick={() => {
+                                    setDisplays(produce(s => {
+                                        const struct = (s[props.index] as OscilloscopeGraphStruct);
+                                        struct.packetsDisplayed[packetIdx()] = !struct.packetsDisplayed[packetIdx()];
+                                    }));
+                                    store.set("display", displays);
+                                }}>
+                                <img alt="Dropdown" src={dropdownIcon}
+                                    class={`h-4 dark:invert`}
+                                    style={`transform: rotate(${displays[props.index]?.packetsDisplayed[packetIdx()] ? "0deg" : "270deg"});`}
+                                    draggable={false} />
+                                <h3 class='font-bold'>{PacketStructureViewModel.name}</h3>
+                            </div>
+
+                            <Show when={displays[props.index]?.packetsDisplayed[packetIdx()]}>
+                                <div class='flex bg-neutral-200 dark:bg-gray-700 p-4 pt-0 pb-0 rounded-lg'>
+                                    <div class='flex flex-col bg-neutral-200 dark:bg-gray-700 p-4 rounded-lg'>
+                                        <h2 class="font-bold">X-Axis</h2>
+                                        <For each={PacketStructureViewModel.components.filter(component => component.type === PacketComponentType.Field)}>
+                                            {(packetComponent: PacketComponent) => {
+                                                const field = packetComponent.data as PacketField;
+                                                return (
+                                                    <label class="flex items-center space-x-2">
+                                                        <input type="radio"
+                                                            class="form-radio"
+                                                            checked={displayStruct.x === field.index && displayStruct.packetID === PacketStructureViewModel.id} // Check based on the state
+                                                            onClick={(event) =>
+                                                                handleSelectX((event.target as HTMLInputElement).checked, field.index, props.index, PacketStructureViewModel.id)
+                                                            }
+                                                        />
+                                                        {field.name}
+                                                    </label>
+                                                );
+                                            }}
+                                        </For>
+                                    </div>
+
+                                    <div class='flex flex-col bg-neutral-200 dark:bg-gray-700 p-4'>
+                                        <h2 class="font-bold">Y-Axis</h2>
+                                        <For each={PacketStructureViewModel.components.filter(component => component.type === PacketComponentType.Field)}>
+                                            {(packetComponent: PacketComponent) => {
+                                                const field = packetComponent.data as PacketField;
+                                                return (
+                                                    <label class="flex items-center space-x-2">
+                                                        <input type="checkbox"
+                                                            class="form-checkbox"
+                                                            checked={displayStruct.y.some(selectedField => selectedField === field.index) && displayStruct.packetID === PacketStructureViewModel.id}
+                                                            onClick={(event) => {
+                                                                handleSelectY((event.target as HTMLInputElement).checked, field.index, props.index, PacketStructureViewModel.id);
+                                                            }} />
+                                                        {field.name}
+                                                    </label>
+                                                );
+                                            }}
+                                        </For>
+                                    </div>
+                                </div>
+                            </Show>
+                        </div>
+                    }
+                </For>
+            </div>
+        </DefaultModalLayout>
+    );
+};
+
+export default OscilloscopeGraphSettingsModal;
