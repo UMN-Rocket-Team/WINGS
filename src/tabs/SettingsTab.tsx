c270628b18462765788d86e789469f78e9fc202e
Joe Anderson <and10287@umn.edu>
2025-05-27 17:17:18 -0500

    clear displays button change

--- src/tabs/SettingsTab.tsx
+++ src/tabs/SettingsTab.tsx
@@ -128,7 +128,7 @@ const SettingsTab: Component = () => {
                             font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 
                             dark:border-gray-700 dark:text-white"
                     >
-                        Clear Display Setup
+                        Remove All Displays
                     </button>
                 </div>
             </footer>
