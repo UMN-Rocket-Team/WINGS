39518328756a6d7da3dd4d1a969870ff40dd200c
oscar.kanestewart <oscar.kanestewart@ligo.org>
2026-02-05 18:41:17 -0600

    edited error message

--- src-tauri/src/communication_manager.rs
+++ src-tauri/src/communication_manager.rs
@@ -214,10 +214,10 @@ impl CommunicationManager {
         for packet in return_buffer.iter() {
         let result = log.write_packet(packet.clone(), &mut *ps_guard);
         if result.is_err() {
-            let new_result = result.unwrap_err().context("failed to write raw data");
+            let new_result = result.unwrap_err().context("failed to export csv");
             let context = new_result.chain();
             for i in context {
-                eprintln!("Binary File Write{:#?}", i);
+                eprintln!("CSV File Export{:#?}", i);
             }
         }
     }
